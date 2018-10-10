8826550861b2234217edd491d7e7125172c4da05
Du Toit <dutoit.minnaar@accelo.com>
2018-10-10 15:37:22 +1100

    Use spaces instead of tabs

--- src/symbols.ts
+++ src/symbols.ts
@@ -50,8 +50,8 @@ export class PerlSymbolProvider
         query: string,
         token: vscode.CancellationToken
     ): Promise<vscode.SymbolInformation[]> {
-		if (query.length <= 2) {
-			return [];
+        if (query.length <= 2) {
+            return [];
         }
 
         let symbols: vscode.SymbolInformation[] = [];
@@ -79,7 +79,7 @@ export class PerlSymbolProvider
                         kind = vscode.SymbolKind.Variable;
                     }
 
-					if (kind == vscode.SymbolKind.Function && name.match(queryRegEx)) {
+                    if (kind == vscode.SymbolKind.Function && name.match(queryRegEx)) {
                         let lineNo = parseInt(match[2].replace(/[^\d]/g, "")) - 1;
 
                         let range = new vscode.Range(lineNo, 0, lineNo, 0);
